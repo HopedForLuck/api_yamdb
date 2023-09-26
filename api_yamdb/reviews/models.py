5a454efe4789a50b0b20894dd6bcd59b3cc61a52
Владимир Орехов <vo56514@yandex.ru>
2023-09-26 11:33:23 +0300

    25/09

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name="Произведение",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name="Автор отзыва",
+    )
+    text = models.TextField(max_length=200)
+    rating = models.PositiveSmallIntegerField(
+        on_delete=models.CASCADE,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        related_name='reviews',
+        help_text="Оцените произведение по шкале от 1 до 10.",
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления', 
+    )
+
+    class Meta:
+        unique_together = ('title', 'rating')
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField(max_length=200)
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления',
+    )
